5ae328ebd09cb6e7b1b76f76705f4b95c9e31d65
Arturas Smorgun <arturas@smorgun.lt>
2015-03-21 23:46:20 +0200

    Refactor infix converter

--- src/Elecode/ExpressionParsing/InfixConverter.php
+++ src/Elecode/ExpressionParsing/InfixConverter.php
@@ -2,56 +2,58 @@
 
 namespace Elecode\ExpressionParsing;
 
+use Elecode\ExpressionParsing\Expression\Symbol;
 use Elecode\ExpressionParsing\Expression\Symbol\Operand;
 use Elecode\ExpressionParsing\Expression\Symbol\Operator;
 use Elecode\ExpressionParsing\Expression\Parser;
 
 class InfixConverter
 {
-    public function toPostfix(Expression $infixExpression)
-    {
-        $postfixExpression = new Expression();
-        $stackOfOperators = array();
-        foreach ($infixExpression->getSequence() as $element) {
-            if ($element instanceof Operand) {
-                $postfixExpression->add($element);
-            } elseif (!count($stackOfOperators)) {
-                array_push($stackOfOperators, $element);
-            } else {
-                /** @var Operator $oldOperator */
-                for (
-                    $oldOperator = array_pop($stackOfOperators);
-                    $oldOperator instanceof Operator;
-                    $oldOperator = array_pop($stackOfOperators)
-                ) {
-                    /** @var Operator $element */
-                    if ($element->hasPrecedenceOver($oldOperator)) {
-                        array_push($stackOfOperators, $oldOperator);
-                        array_push($stackOfOperators, $element);
-                        break;
-                    } else {
-                        $postfixExpression->add($oldOperator);
-                        if (!count($stackOfOperators)) {
-                            array_push($stackOfOperators, $element);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        for (
-            $operator = array_pop($stackOfOperators);
-            $operator instanceof Operator;
-            $operator = array_pop($stackOfOperators)
-        ) {
-            $postfixExpression->add($operator);
-        }
-        return $postfixExpression;
-    }
-
     public function toPostfixString($string)
     {
         $infixExpression = Parser::parseExpression($string);
         return (string) $this->toPostfix($infixExpression);
+    }
+
+    public function toPostfix(Expression $infixExpression)
+    {
+        $postfixExpression = new Expression();
+        $operators = new Stack();
+        foreach ($infixExpression->getSequence() as $symbol) {
+            if ($symbol instanceof Operand) {
+                $postfixExpression->add($symbol);
+            } else {
+                $this->handleOperator($postfixExpression, $operators, $symbol);
+            }
+        }
+        $this->addAllRemainingStackOperators($postfixExpression, $operators);
+        return $postfixExpression;
+    }
+
+    private function handleOperator(Expression $expression, Stack $operators, Operator $operator)
+    {
+        if (!$operators->isEmpty()) {
+            $this->addAllOperatorsWithNotLowerPrecedence($expression, $operators, $operator);
+        }
+        $operators->push($operator);
+    }
+
+    private function addAllOperatorsWithNotLowerPrecedence(Expression $expression, Stack $operators, Operator $operator)
+    {
+        for ($stackOperator = $operators->pop(); !is_null($stackOperator); $stackOperator = $operators->pop()) {
+            if ($operator->hasPrecedenceOver($stackOperator)) {
+                $operators->push($stackOperator);
+                break;
+            } else {
+                $expression->add($stackOperator);
+            }
+        }
+    }
+
+    private function addAllRemainingStackOperators(Expression $expression, Stack $operators)
+    {
+        for ($operator = $operators->pop(); !is_null($operator); $operator = $operators->pop()) {
+            $expression->add($operator);
+        }
     }
 }
